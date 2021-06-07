5499b6ffad83d65846a99227e887ed3614ceb29e
Russell Bennett <russell@ipgnosis.com>
2021-06-07 08:34:43 -0700

    Cleaned up logic in function like

--- like.py
+++ like.py
@@ -13,8 +13,6 @@ def like(strA, strB, *args):
     lenA = len(strA)
     lenB = len(strB)
 
-    # print('lenA = {}, lenB = {}'.format(lenA, lenB))
-
     tally = 0.0
     incr = 0.1
 
@@ -24,11 +22,18 @@ def like(strA, strB, *args):
         for posn1 in range(len(strA)):
             if strA[posn1] == strB[posn1]:
                 tally += incr
+    else:
+        # inequal string lengths results in index error from a for loop
+        # need a way to evaluate strings of inequal length
+        print('len({}) = {}; len({}) = {}'.format(strA, lenA, strB, lenB))
 
-    if args and tally >= threshold:
-        return True
-
-    return tally
+    if args:
+        if tally >= threshold:
+            return True
+        else:
+            return False
+    else:
+        return tally
 
 
 def main():
@@ -40,24 +45,22 @@ def main():
 
     # str1 = 'Kazakhstan'
     # str2 = 'Khazakstan'
+    # no_args = like(str1, str2)
+    # with_args = like(str1, str2, 0.9)
+    # print('no_args =', no_args)
+    # print('with_args =', with_args)
 
+    # get test data
     test_data = "test_data.json"
     CURRENT_DIR_STR = os.path.abspath('')
     test_data_path = os.path.join(CURRENT_DIR_STR, 'data', test_data)
 
     data = read_json_data(test_data_path)
 
+    # run function like against test data
     if data:
         for lis in data['test_data']:
             print("{}, {}, {}".format(lis[0], lis[1], like(lis[0], lis[1], 0.9)))
-
-    # no_args = like(str1, str2)
-
-    # with_args = like(str1, str2, 0.9)
-
-    # print('no_args =', no_args)
-
-    # print('with_args =', with_args)
 
 
 if __name__ == "__main__":
