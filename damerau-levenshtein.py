5ae2293226d88c75125ba7b1f79d402eecc92d22
Russell Bennett <russell@ipgnosis.com>
2021-11-05 13:21:05 -0700

    Create damerau-levenshtein.py

---
+++ damerau-levenshtein.py
@@ -0,0 +1,46 @@
+#  implements Damerau-Levenshtein algo for measuring the edit distance between two strings
+
+def damerau_levenshtein(strA, strB, limit):
+    '''Returns True or False, depending on whether strA and strB are within the limit param'''
+    pass
+
+#  pseudo-code
+"""
+algorithm DL-distance is
+    input: strings a[1..length(a)], b[1..length(b)]
+    output: distance, integer
+    
+    da := new array of |Σ| integers
+    for i := 1 to |Σ| inclusive do
+        da[i] := 0
+    
+    let d[−1..length(a), −1..length(b)] be a 2-d array of integers, dimensions length(a)+2, length(b)+2
+    // note that d has indices starting at −1, while a, b and da are one-indexed.
+    
+    maxdist := length(a) + length(b)
+    d[−1, −1] := maxdist
+    for i := 0 to length(a) inclusive do
+        d[i, −1] := maxdist
+        d[i, 0] := i
+    for j := 0 to length(b) inclusive do
+        d[−1, j] := maxdist
+        d[0, j] := j
+    
+    for i := 1 to length(a) inclusive do
+        db := 0
+        for j := 1 to length(b) inclusive do
+            k := da[b[j]]
+            ℓ := db
+            if a[i] = b[j] then
+                cost := 0
+                db := j
+            else
+                cost := 1
+            d[i, j] := minimum(d[i−1, j−1] + cost,  //substitution
+                               d[i,   j−1] + 1,     //insertion
+                               d[i−1, j  ] + 1,     //deletion
+                               d[k−1, ℓ−1] + (i−k−1) + 1 + (j-ℓ−1)) //transposition
+        da[a[i]] := i
+    return d[length(a), length(b)]
+    
+    """
